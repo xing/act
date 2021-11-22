7d20763e27324e964e9679890d0f06a4ef9270e9
Markus Wolf <mail@markus-wolf.de>
2021-11-22 11:33:57 +0100

    patch: update owner name from nektos to xing
    - installer update
    - test asserts

--- pkg/runner/run_context_test.go
+++ pkg/runner/run_context_test.go
@@ -373,12 +373,12 @@ func TestGetGitHubContext(t *testing.T) {
 		actor = a
 	}
 
-	repo := "nektos/act"
+	repo := "xing/act"
 	if r := os.Getenv("ACT_REPOSITORY"); r != "" {
 		repo = r
 	}
 
-	owner := "nektos"
+	owner := "xing"
 	if o := os.Getenv("ACT_OWNER"); o != "" {
 		owner = o
 	}

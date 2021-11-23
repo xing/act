a648ba963f0f3cdc22fb6bba91f8ee1e0b5f2bcd
Björn Brauer <bjoern.brauer@new-work.se>
2021-11-23 10:36:03 +0000

    chore: silence curl and script output from apply-patches.sh
    Co-authored-by: Markus Wolf <markus.wolf@new-work.se>

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 cd act || exit 1
 
 grep -v '^#' < ../patches | while IFS= read -r patch
 do
-    echo "Applying patch $patch"
-    curl -L "$patch" | git am -3 || exit 1
+    echo "Patch from: $patch"
+    curl -sL "$patch" | git am -3 || exit 1
 done

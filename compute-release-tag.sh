3867e603c796475486d54eec2e5b50bfa85c0c7c
ZauberNerd <zaubernerd@zaubernerd.de>
2023-08-16 16:46:14 +0200

    chore: replace deprecated set-output with GITHUB_OUTPUT

--- compute-release-tag.sh
+++ compute-release-tag.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 version=$(cd act && git describe --tags --dirty --always |cut -d "-" -f 1)
 hash=$(./version-hash.sh)
 versionHash=${version}-${hash}
-echo "::set-output name=versionHash::${versionHash}"
+echo "versionHash=${versionHash}" >> $GITHUB_OUTPUT
 
 gitTag=$(git for-each-ref --sort=taggerdate --format '%(tag)' refs/tags |tail -1)
 # shellcheck disable=SC2001
@@ -16,12 +16,12 @@ else
   counter=$(("${counter}" + 1))
 fi
 tag="${version}-xing.${counter}-${hash}"
-echo "::set-output name=tag::${tag}"
+echo "tag=${tag}" >> $GITHUB_OUTPUT
 
 latestHash="$(curl -fsSLI -o /dev/null -w "%{url_effective}" https://github.com/xing/act/releases/latest)"
 latestHash="${latestHash#https://github.com/xing/act/releases/tag/}"
 # shellcheck disable=SC2001
 latestHash=$(echo "${latestHash}" |sed -e 's/-xing\.[[:digit:]]\+//')
-echo "::set-output name=latestHash::${latestHash}"
+echo "latestHash=${latestHash}" >> $GITHUB_OUTPUT
 
 echo "versionHash: ${versionHash} tag: ${tag} latestHash: ${latestHash}"
